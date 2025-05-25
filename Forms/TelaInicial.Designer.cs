117ab68c3d686c4f89dcc3eb214d48ae6ea1dbc0
Caio Matheus de Oliveira <caiomathol@gmail.com>
2025-05-24 22:46:36 -0300

    tela inicial

---
+++ Forms/TelaInicial.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace pitaya_crud.Forms
+{
+    partial class TelaInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaInicial));
+            clientesButton = new Button();
+            comprasButton = new Button();
+            produtosButton = new Button();
+            usuariosButton = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            graficoPanel = new Panel();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // clientesButton
+            // 
+            clientesButton.BackColor = Color.LightSkyBlue;
+            clientesButton.Image = (Image)resources.GetObject("clientesButton.Image");
+            clientesButton.Location = new Point(648, 231);
+            clientesButton.Name = "clientesButton";
+            clientesButton.Size = new Size(140, 82);
+            clientesButton.TabIndex = 0;
+            clientesButton.UseVisualStyleBackColor = false;
+            clientesButton.Click += clientesButton_Click;
+            // 
+            // comprasButton
+            // 
+            comprasButton.BackColor = Color.IndianRed;
+            comprasButton.Image = (Image)resources.GetObject("comprasButton.Image");
+            comprasButton.Location = new Point(648, 12);
+            comprasButton.Name = "comprasButton";
+            comprasButton.Size = new Size(140, 82);
+            comprasButton.TabIndex = 1;
+            comprasButton.UseVisualStyleBackColor = false;
+            comprasButton.Click += comprasButton_Click;
+            // 
+            // produtosButton
+            // 
+            produtosButton.BackColor = Color.LightSkyBlue;
+            produtosButton.Image = (Image)resources.GetObject("produtosButton.Image");
+            produtosButton.Location = new Point(648, 119);
+            produtosButton.Name = "produtosButton";
+            produtosButton.Size = new Size(140, 82);
+            produtosButton.TabIndex = 2;
+            produtosButton.UseVisualStyleBackColor = false;
+            produtosButton.Click += produtosButton_Click;
+            // 
+            // usuariosButton
+            // 
+            usuariosButton.BackColor = Color.IndianRed;
+            usuariosButton.Image = (Image)resources.GetObject("usuariosButton.Image");
+            usuariosButton.Location = new Point(648, 336);
+            usuariosButton.Name = "usuariosButton";
+            usuariosButton.Size = new Size(140, 82);
+            usuariosButton.TabIndex = 3;
+            usuariosButton.UseVisualStyleBackColor = false;
+            usuariosButton.Click += usuariosButton_Click;
+            // 
+            // graficoPanel
+            // 
+            graficoPanel.BackColor = Color.Transparent;
+            graficoPanel.Location = new Point(51, 68);
+            graficoPanel.Name = "graficoPanel";
+            graficoPanel.Size = new Size(497, 322);
+            graficoPanel.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSkyBlue;
+            button1.Location = new Point(51, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Reiniciar ";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // TelaInicial
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(graficoPanel);
+            Controls.Add(usuariosButton);
+            Controls.Add(produtosButton);
+            Controls.Add(comprasButton);
+            Controls.Add(clientesButton);
+            Name = "TelaInicial";
+            Text = "TelaInicial";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button clientesButton;
+        private Button comprasButton;
+        private Button produtosButton;
+        private Button usuariosButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Panel graficoPanel;
+        private Button button1;
+    }
+}
---
+++ Forms/TelaInicial.cs
@@ -0,0 +1,121 @@
+﻿
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.WinForms;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView.Painting;
+using pitaya_crud.Models;
+using pitaya_crud.Services;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView;
+
+namespace pitaya_crud.Forms
+{
+    public partial class TelaInicial : Form
+    {
+        private CartesianChart graficoVendas;
+        private readonly CompraService _compraService;
+
+        public TelaInicial()
+        {
+            InitializeComponent();
+            _compraService = new CompraService();
+            InicializarGrafico();
+            _ = CarregarDadosDeVendas();
+        }
+
+        private void InicializarGrafico()
+        {
+            graficoVendas = new CartesianChart
+            {
+                Dock = DockStyle.Fill,
+                LegendPosition = LegendPosition.Top,
+                TooltipPosition = TooltipPosition.Top,
+            };
+
+            graficoPanel.Controls.Add(graficoVendas);
+        }
+
+        private async Task CarregarDadosDeVendas()
+        {
+            List<Compra> compras = await _compraService.GetComprasAsync();
+
+            var dadosAgrupados = compras
+                .GroupBy(c => new DateTime(c.Data.Year, c.Data.Month, 1))
+                .OrderBy(g => g.Key)
+                .ToList();
+
+            if (dadosAgrupados.Count == 0)
+            {
+                graficoVendas.Series = Array.Empty<ISeries>();
+                graficoVendas.XAxes = Array.Empty<Axis>();
+                graficoVendas.YAxes = Array.Empty<Axis>();
+                return;
+            }
+
+            var labels = dadosAgrupados.Select(g => g.Key.ToString("MMM yyyy")).ToArray();
+            var values = dadosAgrupados.Select(g => (double)g.Sum(c => c.Total)).ToArray();
+
+            var series = new LineSeries<double>
+            {
+                Name = "Vendas",
+                Values = values,
+                Fill = new SolidColorPaint(new SKColor(70, 130, 180, 100)),
+                Stroke = new SolidColorPaint(new SKColor(70, 130, 180)) { StrokeThickness = 3 },
+                GeometrySize = 0,
+                LineSmoothness = 0.3,
+            };
+
+
+            graficoVendas.Series = [series];
+
+            graficoVendas.XAxes =
+            [
+                new Axis
+                {
+                    Labels = labels,
+                    LabelsRotation = 45,
+                    Name = "Mês",
+                    NamePadding = new LiveChartsCore.Drawing.Padding(0, 15, 0, 0),
+                }
+            ];
+
+            graficoVendas.YAxes =
+            [
+                new Axis
+                {
+                    Name = "Total de Vendas (R$)",
+                    Labeler = value => value.ToString("C2")
+                }
+            ];
+        }
+
+        private void comprasButton_Click(object sender, EventArgs e)
+        {
+            new TelaCompra().ShowDialog();
+            Show();
+        }
+
+        private void produtosButton_Click(object sender, EventArgs e)
+        {
+            new TelaProduto().ShowDialog();
+            Show();
+        }
+
+        private void clientesButton_Click(object sender, EventArgs e)
+        {
+            new TelaListagem().ShowDialog();
+            Show();
+        }
+
+        private void usuariosButton_Click(object sender, EventArgs e)
+        {
+            // new TelaUsuario.ShowDialog();
+            // Show();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await CarregarDadosDeVendas();
+        }
+    }
+}
